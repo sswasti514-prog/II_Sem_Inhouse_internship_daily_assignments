02fc48bf3f27eb65cb7cb3506c06461988be34af
swastisharma <sswasti514@gmail.com>
2026-07-10 17:28:38 +0530

    ASSIGNMENT-7
    APPLE BANANA GAME

--- script.js
+++ script.js
@@ -1,52 +1,97 @@
-const movies={
+const fruit=document.getElementById("fruit");
+const basket=document.getElementById("basket");
+const scoreText=document.getElementById("score");
 
-aishwarya:{
+let basketX=250;
+let fruitX=200;
+let fruitY=-50;
 
-name:"Ponniyin Selvan: Part II (2023)",
+let score=0;
 
-image:"ponniyin.jpg.jpeg"
+let speed=4;
 
-},
+let currentFruit="🍎";
 
-aditya:{
+document.addEventListener("keydown",(e)=>{
 
-name:"Article 370 (2024)",
+if(e.key=="ArrowLeft" && basketX>0){
 
-image:"article370.jpg.jpeg"
-
-},
-
-aamir:{
-
-name:"Sitaare Zameen Par (2025)",
-
-image:"sitarezameenpar.jpg.jpeg"
-
-},
-
-hrithik:{
-
-name:"Fighter (2024)",
-
-image:"fighter.jpg.jpeg"
-
-},
-
-kajol:{
-
-name:"Maa (2025)",
-
-image:"maa.jpg.jpeg"
+basketX-=25;
 
 }
 
-};
+if(e.key=="ArrowRight" && basketX<590){
 
-
-function showMovie(actor){
-
-document.getElementById("movieImage").src=movies[actor].image;
-
-document.getElementById("movieName").innerHTML=movies[actor].name;
+basketX+=25;
 
 }
+
+basket.style.left=basketX+"px";
+
+});
+
+function newFruit(){
+
+currentFruit=Math.random()>0.5?"🍎":"🍌";
+
+fruit.innerHTML=currentFruit;
+
+fruitX=Math.random()*620;
+
+fruitY=-60;
+
+fruit.style.left=fruitX+"px";
+
+if(currentFruit=="🍎"){
+
+basket.style.width="90px";
+
+}
+
+else{
+
+basket.style.width="150px";
+
+}
+
+}
+
+newFruit();
+
+function gameLoop(){
+
+fruitY+=speed;
+
+fruit.style.top=fruitY+"px";
+
+let basketWidth=parseInt(getComputedStyle(basket).width);
+
+if(
+
+fruitY>500 &&
+
+fruitX>basketX &&
+
+fruitX<(basketX+basketWidth)
+
+){
+
+score++;
+
+scoreText.innerHTML="Score : "+score;
+
+newFruit();
+
+}
+
+if(fruitY>620){
+
+newFruit();
+
+}
+
+requestAnimationFrame(gameLoop);
+
+}
+
+gameLoop();
